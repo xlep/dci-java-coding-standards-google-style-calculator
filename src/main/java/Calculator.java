692f957de61eec325ca857318e43d1f4d01dd736
Philipp Borst <borst.philipp@gmail.com>
2021-07-28 13:43:44 +0200

    changed the example to fit our style ruels

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,58 +1,71 @@
 import java.util.Scanner;
 
+/**
+ * A calculator class for simple arithmetics.
+ */
 public class Calculator {
-  public static void main(String[] args)
-  {
-    System.out.println("Enter first and second number:");
-    Scanner INPUT= new Scanner(System.in);
 
-    int num1,num2;
-    num1 = INPUT.nextInt();
-    num2 = INPUT.nextInt();
+  /**
+   * The main method queries for two numbers and an arithmetic operation. Then returns the result.
+   */
+  public static void main(String[] args)  {
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Enter first and second number:");
+    int num1 = input.nextInt();
+    int num2 = input.nextInt();
 
     System.out.println("Enter the operation: + for addition, - for subtraction, * for multiplication or / for division:");
-    String calc_operation;
-    calc_operation = INPUT.next();
+    String calcOperation;
+    calcOperation = input.next();
 
-    switch (calc_operation){
+    switch (calcOperation) {
       case "+":
-        System.out.println(add( num1,num2));
+        System.out.println(add(num1,num2));
         break;
       case "-":
-        System.out.println(sub( num1,num2));
+        System.out.println(sub(num1,num2));
         break;
       case "*":
-        System.out.println(Multiply( num1,num2));
+        System.out.println(multiply(num1,num2));
         break;
       case "/":
-        System.out.println(divide_numbers( num1,num2));
+        System.out.println(divideNumbers(num1,num2));
         break;
       default:
         System.out.println("No operation selected");
     }
   }
 
-  public static int add(int x, int y)
-  {
+  /**
+   * Method for the addition operation.
+   */
+  public static int add(int x, int y) {
     int result = x + y;
     return result;
   }
 
-  public static int sub(int x, int y)
-  {
-    int result = x-y;
+  /**
+   * Method for the subtraction operation.
+   */
+  public static int sub(int x, int y) {
+    int result = x - y;
     return result;
   }
 
-  public static int Multiply(int x, int y)
-  {
-    int result = x*y;
+  /**
+   * Method for the multiplication operation.
+   */
+  public static int multiply(int x, int y) {
+    int result = x * y;
     return result;
   }
 
-  public static int divide_numbers(int x, int y)
-  {
-    int result = x/y;
+  /**
+   * Method for the division operation.
+   */
+  public static int divideNumbers(int x, int y) {
+    int result = x / y;
     return result;
   }
 
